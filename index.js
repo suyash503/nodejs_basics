67284a8083ae40e12281d92120d4f8e988c421ab
Suyash <suyashsingh2711@gmail.com>
2025-06-28 18:49:37 +0530

    practice

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const math = require('./math')
-console.log(math(5, 6));
+const os = require('os');
+console.log(os.cpus);
 
+console.log(math.add(5, 6));
+console.log(math.sub(5, 6));
 const fs = require('fs');
+fs.writeFile('text.txt', 'hello world', (err) => { });
+fs.readFile('./text.txt', 'utf-8', (err, result) => {
+    if (err) {
+        console.log('error', err);
+    } else {
+        console.log(result);
+    }
+});
--- math.js
+++ math.js
@@ -1,5 +1,11 @@
 function add(a, b) {
     return (a+b)
 }
+function sub(a, b) {
+    return (a-b)
+}
 
-module.exports = add;
+module.exports = {
+    add,
+    sub,
+}
