b8a06cf5f94875c44f0ac6ef745326e94c2e2fa5
Suyash <suyashsingh2711@gmail.com>
2025-06-29 19:17:36 +0530

    ..

--- http_server/server.js
+++ http_server/server.js
@@ -1,9 +1,15 @@
 const http = require('http');
+const fs = require('fs');
 const myServer = http.createServer((req, res) => {
-    console.log("new rew rec ");
-    res.sendDate("hello from server");
+    const log = `${Date.now()}: new req received \n`;
+    fs.appendFile("log.txt", log, (err, data) => {
+        res.end("hello from server again");
+    });
+    console.log("new request received"); //ye part jitni br page 
+    console.log(req.headers);           //refresh hoga utni br chalega {terminal me}
+    res.end("hello from server");       //{page pr display hoga}
 });
 
-myServer.listen(8000, () => {
+myServer.listen(8900, () => {
     console.log("server started"); //if server is working fine this msg will be displayed
 })
