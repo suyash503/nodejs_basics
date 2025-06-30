5760762f223966ff20718c412aab500469315725
Suyash <suyashsingh2711@gmail.com>
2025-06-30 12:43:47 +0530

    ..

---
+++ project/backend/data.js
@@ -0,0 +1,6 @@
+const menu = [
+    { id: 1, name: 'paneer chilli', price: '400' },
+    { id: 2, name: 'paneer do pyaza', price: '200' },
+    { id: 3, name: 'shahi paneer', price: '600' },
+    { id: 4, name: 'afghani chaap', price: '300' },
+]
